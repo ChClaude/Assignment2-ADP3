e78c339487e481a35f7ac9d1e94451832ccb3998
ChClaude <christ.tchambila@gmail.com>
2020-03-11 10:40:05 +0200

    assigments completed

--- src/test/java/za/ac/cput/adp3/AppTest.java
+++ src/test/java/za/ac/cput/adp3/AppTest.java
@@ -1,8 +1,9 @@
 package za.ac.cput.adp3;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -20,7 +21,32 @@ public class AppTest
 
     @Test
     public void testObjectEquality() {
+        String name = "Leo";
+        assertEquals(name, "Leo");
+    }
 
+    @Test
+    public void testObjectIdentity() {
+        String name = new String("John Doe");
+        assertSame(name, name);
+    }
+
+    @Test
+    public void testFails() {
+        String name = new String("John Doe");
+        assertSame(name, "John Doe");
+    }
+
+    @Test(timeout = 1)
+    public void testTimeouts() {
+        System.out.println("Inside timeout test");
+    }
+
+    @Ignore
+    @Test
+    public void testDisabling() {
+        String name = new String("John Doe");
+        assertSame(name, "John Doe");
     }
 
 }
