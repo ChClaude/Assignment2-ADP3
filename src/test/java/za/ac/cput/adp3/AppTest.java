5728c8b48be3ae760ede716b77c816b9cd5c7b12
ChClaude <christ.tchambila@gmail.com>
2020-03-11 10:25:28 +0200

    first commit

---
+++ src/test/java/za/ac/cput/adp3/AppTest.java
@@ -0,0 +1,26 @@
+package za.ac.cput.adp3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testObjectEquality() {
+
+    }
+
+}
